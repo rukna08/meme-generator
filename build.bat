932a15b7f64d136f668e6eb22aa49895d03d1bc8
Ankur Banerjee <rukna2003@gmail.com>
2023-11-15 14:31:16 +0530

    Changed build commands and changed main.c to meme_generator.c

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ if not exist "build" mkdir build
 
 pushd build
 
-cl -Zi ../src/main.c user32.lib gdi32.lib kernel32.lib
+cl /Zi /Fe:meme_generator.exe ../src/*.c user32.lib gdi32.lib kernel32.lib
 
 popd
